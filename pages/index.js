db46301e7e57838932500ca3332c6dd9685429cb
Yash Mehta <yashmehta0395@gmail.com>
2023-01-08 20:26:37 +0530

    added mui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import BaseLayout from "../layouts";
+import { Button } from "@mui/material";
+import ComboBox from "../components/Common/Inputs/ComboBox";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,9 +10,9 @@ export default function Home() {
   return (
     <>
       <BaseLayout>
-        <h1>
-          Home Page
-        </h1>
+        <h1>Home Page</h1>
+        <Button variant="contained">Hello World</Button>
+        <ComboBox />
       </BaseLayout>
     </>
   );
